b660415995eb4155ff560fc07f3d480cb1605cb2
Dacyto <55278341+Dacyto@users.noreply.github.com>
2022-03-10 23:12:19 +0300

    1-st function realization

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,7 +2,26 @@
 #include "fun.h"
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    
+   int i = 0;
+   int count = 0;
+   bool numb = true;
+   bool inWord = false;
+    
+    while (str[i]) {
+        if (str[i] != ' ' && inWord == false) {
+            inWord = true;
+            if(inWord == true && isdigit(str[i]) == 0)
+               numb = false;
+        }
+        else if (str[i] == ' ' && inWord == true && numb == false) {
+               count++;
+               inWord = false;
+               numb = true;
+        }
+    i++;    
+    }
+    return count;
 }
 
 unsigned int faStr2(const char *str) {
