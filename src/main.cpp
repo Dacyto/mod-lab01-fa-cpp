620604bc7d1612804c928fe9533a9c7de5bc25f3
Dacyto <55278341+Dacyto@users.noreply.github.com>
2022-03-10 23:19:15 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main() {
     unsigned int result1 = faStr1(str);
     unsigned int result2 = faStr2(str);
     unsigned int result3 = faStr3(str);
-    cout << "Result of 1-st function = " cout << result1 endl;
-    cout << "Result of 2-nd function = " cout << result2 endl;
-    cout << "Result of 3-rd function = " cout << result3 endl;
+    cout << "Result of 1-st function = " cout << result1 << endl;
+    cout << "Result of 2-nd function = " cout << result2 << endl;
+    cout << "Result of 3-rd function = " cout << result3 << endl;
 }
