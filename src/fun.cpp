96d30c104f86d08fd2a8589bcd545cb641415247
Dacyto <55278341+Dacyto@users.noreply.github.com>
2022-03-10 23:59:07 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -4,20 +4,24 @@
 unsigned int faStr1(const char *str) {
 int i = 0;
 int count = 0;
-bool numb = true;
+bool numb = false;
 bool inWord = false;
     
 while (str[i]) {
         if (str[i] != ' ' && inWord == false) {
             inWord = true;
-            if (inWord == true && isdigit(str[i]) == 0)
-               numb = false;
+            if (inWord == true && isdigit(str[i]))
+               numb = true;
         }
-        else if (str[i] == ' ' && inWord == true && numb == false) {
+        else if (str[i] == ' ' && inWord == true && numb == true) {
         count++;
         inWord = false;
-        numb = true;
+        numb = false;
         }
+        else if (str[i] == ' ' && inWord == true) {
+        inWord = false;
+        numb = false;
+        }    
     i++;    
 }
     return count;
