005ab40eeb7ae8ea225951c7e09a3ea94030c57c
Dacyto <55278341+Dacyto@users.noreply.github.com>
2022-03-11 12:05:49 +0300

    2-nd function realization

--- src/fun.cpp
+++ src/fun.cpp
@@ -25,7 +25,31 @@ while (str[i]) {
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+    int i = 0;
+    int count = 0;
+    bool inWord = false;
+    bool Zaglav_char = false;
+    bool eng_lowercase_char = false;
+    while (str[i]) {
+        if (str[i] != ' ') {
+            inWord = true;
+            if (inWord == true && Zaglav_char == false && eng_lowercase_char == false && str[i] >= 'A' && str[i] <= 'Z')
+               Zaglav_char = true;
+            if (inWord == true && Zaglav_char == true && str[i] >= 'a' && str[i] <= 'z')
+               eng_lowercase_char = true;
+        } else if (str[i] == ' ' && inWord == true && Zaglav_char == true && eng_lowercase_char == true) {
+        count++;
+        inWord = false;
+        Zaglav_char = false;
+        eng_lowercase_char = false;
+        } else if (str[i] == ' ' && inWord == true) {
+        inWord = false;
+        Zaglav_char = false;
+        eng_lowercase_char = false;
+        }
+   i++;
+ }
+   return count;
 }
 
 unsigned int faStr3(const char *str) {
